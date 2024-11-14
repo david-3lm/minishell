61819f253681dca15df02b26bbb64972b9fa2ff4
David Lopez Lopez <d.3lopm@gmail.com>
2024-11-14 13:05:31 +0100

    includes

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,14 +6,22 @@
 /*   By: dlopez-l <dlopez-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 11:38:48 by dlopez-l          #+#    #+#             */
-/*   Updated: 2024/11/14 12:41:41 by dlopez-l         ###   ########.fr       */
+/*   Updated: 2024/11/14 13:04:44 by dlopez-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 # include "../libimp/libft.h"
-# include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <termios.h>
+# include <curses.h>
+# include <unistd.h>
+# include <string.h>
+# include <dirent.h>
+# include <stdio.h>
+# include <term.h>
 #endif
