f20d5a06e534cc2fed81c6f68bd72fe91106a093
David Lopez Lopez <dlopez-l@c3r9s5.42madrid.com>
2025-04-08 12:54:07 +0200

    echo

--- src/built-ins/bi_echo.c
+++ src/built-ins/bi_echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   bi_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cde-migu <cde-migu@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: dlopez-l <dlopez-l@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 19:03:41 by cde-migu          #+#    #+#             */
-/*   Updated: 2025/04/01 17:31:45 by cde-migu         ###   ########.fr       */
+/*   Updated: 2025/04/08 12:51:02 by dlopez-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,50 @@
 	- si es solo echo sin argumento, salto de linea
 	- una variable para comprobar si tiene o no -n
  */
-
-void    bi_echo(t_cmd_table *table, t_cmd *cmd)
+void	print_lines(t_list *cmd, int out)
 {
-	(void)cmd;
+	char	*str;
+	t_list	*tok;
+	t_cmd	*c;
+
+
+	c = (t_cmd *)cmd->content;
+	tok = c->tokens;
+	while (tok)
+	{
+		str = ((t_tok *)tok->content)->value;
+		if (str)
+			ft_putstr_fd(str, out);
+		tok = tok->next;
+	}
+}
+
+
+int	bi_echo(t_cmd_table *table, t_cmd *cmd)
+{
+	t_list	*next;
+	char	*aux;
+	int		i;
+	bool	n_opt;
+
 	(void)table;
-    printf("holaecho");
+	n_opt = false;
+	next = cmd->tokens->next;
+	aux = (char *)next->content;
+	i = 0;
+	if (aux[i] && aux[i] == '-' && aux[i + 1] == 'n')
+	{
+		i++;
+		while (aux[i] == 'n')
+			i++;
+		if (aux[i] == '\0')
+		{
+			n_opt = true;
+			next = next->next;
+		}
+	}
+	print_lines(next, STDOUT_FILENO);
+	if (n_opt == false)
+		ft_putchar_fd('\n', STDOUT_FILENO);
+	return (EXIT_SUCCESS);
 }
