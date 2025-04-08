1c974fe3bf74caaf3da60d4536c378847edffa2a
Carolina De Miguel Gonzalez <cde-migu@c3r9s4.42madrid.com>
2025-04-08 12:56:24 +0200

    cd sin argumentos

--- src/built-ins/bi_cd.c
+++ src/built-ins/bi_cd.c
@@ -6,7 +6,7 @@
 /*   By: cde-migu <cde-migu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 19:03:41 by cde-migu          #+#    #+#             */
-/*   Updated: 2025/04/01 18:39:04 by cde-migu         ###   ########.fr       */
+/*   Updated: 2025/04/08 12:55:12 by cde-migu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,50 @@ void    bi_cd(t_cmd_table *table, t_cmd *cmd)
 	(void)cmd;
 	(void)table;
     printf("holacd");
+}
+
+/* 
+	- primero coger las variabs de entorno pwd y old pwd
+	- modificar valor de oldpwd por el valor actual antes de cambiar de direccion
+	- si cd no tiene argumento, redirigir a donde señala HOME
+	- 
+ */
+
+void	ft_change_old_pwd(t_cmd_table *table)
+{
+	t_env	*old_pwd;
+
+	old_pwd = mini_get_env(table, "OLDPWD");
+	old_pwd->value = getcwd(old_pwd->value, 0);
+}
+
+int	ft_change_path(t_cmd_table *table, char *arg)
+{
+	int		error_code;
+	char	*path;
+	if !(arg[0])
+	{
+		path = mini_get_env(table, "HOME")->value;
+		ft_change_old_pwd(table);
+		if (!env_path)
+		{
+			// revisar codigo de error por aqui
+			ft_putendl_fd("minishell : cd: HOME not set", STDERR);
+			return (1);
+		}
+	}
+	else if (arg == '-')
+	{
+		path = mini_get_env(table, "OLDPWD")->value;
+		ft_change_old_pwd(table);
+		if (!env_path)
+		{
+			// revisar codigo de error por aqui
+			ft_putendl_fd("minishell : cd: OLDPWD not set", STDERR);
+			return (1);
+		}
+	}
+	// TODO: hacer con path absoluto y relativo
+	ret = chdir(path);
+	return (ret);
 }
--- src/built-ins/bi_env.c
+++ src/built-ins/bi_env.c
@@ -6,7 +6,7 @@
 /*   By: cde-migu <cde-migu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 19:03:41 by cde-migu          #+#    #+#             */
-/*   Updated: 2025/03/25 16:31:00 by cde-migu         ###   ########.fr       */
+/*   Updated: 2025/04/08 12:26:21 by cde-migu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,19 @@ void    bi_env(t_cmd_table *table, t_cmd *cmd)
 		env_lst = env_lst->next;
 	}
 } 
+
+t_env	*mini_get_env(t_cmd_table *table, char *var)
+{
+	t_list	*env_lst;
+	t_env	*env;
+
+	env_lst = table->envv;
+	while (env_lst != NULL)
+	{
+		env = (t_env *)env_lst->content;
+		if (ft_strncmp(var, env->key, ft_strlen(var)) == 0)
+			return (env);
+		env_lst = env_lst->next;
+	}
+	return (NULL);
+}
