48f2653170d16dcc434e759141a9e80ea1618696
caroldmg <carolinadmg98@gmail.com>
2025-04-17 16:12:28 +0200

    info de como hacer el exit

--- src/built-ins/bi_exit.c
+++ src/built-ins/bi_exit.c
@@ -6,7 +6,7 @@
 /*   By: cde-migu <cde-migu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 12:58:31 by cde-migu          #+#    #+#             */
-/*   Updated: 2025/04/14 15:21:44 by cde-migu         ###   ########.fr       */
+/*   Updated: 2025/04/17 16:12:00 by cde-migu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,79 @@ void	bi_exit(t_cmd_table *table, t_cmd *cmd)
 	exit(EXIT_SUCCESS);
 	// exit libera? o tengo que liberar yo cosas?
 }
+
+/* CCLAUDE42
+	void	mini_exit(t_mini *mini, char **cmd)
+{
+	mini->exit = 1;
+	ft_putstr_fd("exit ", STDERR);
+	cmd[1] ? ft_putendl_fd("❤️", STDERR) : ft_putendl_fd("💚", STDERR);
+	if (cmd[1] && cmd[2])
+	{
+		mini->ret = 1;
+		ft_putendl_fd("minishell: exit: too many arguments", STDERR);
+	}
+	else if (cmd[1] && ft_strisnum(cmd[1]) == 0)
+	{
+		mini->ret = 255;
+		ft_putstr_fd("minishell: exit: ", STDERR);
+		ft_putstr_fd(cmd[1], STDERR);
+		ft_putendl_fd(": numeric argument required", STDERR);
+	}
+	else if (cmd[1])
+		mini->ret = ft_atoi(cmd[1]);
+	else
+		mini->ret = 0;
+}
+*/
+
+/* MAIADEGRAAF
+	int	is_str_digit(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	determine_exit_code(char **str)
+{
+	int	exit_code;
+
+	if (!str[1])
+		exit_code = 0;
+	else if (is_str_digit(str[1]))
+		exit_code = ft_atoi(str[1]);
+	else
+	{
+		ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+		ft_putstr_fd(str[1], STDERR_FILENO);
+		ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
+		exit_code = 255;
+	}
+	free_arr(str);
+	exit(exit_code);
+}
+
+int	mini_exit(t_tools *tools, t_simple_cmds *simple_cmd)
+{
+	char	**str;
+
+	ft_putendl_fd("exit", STDERR_FILENO);
+	if (simple_cmd->str[1] && simple_cmd->str[2])
+	{
+		ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
+		return (EXIT_FAILURE);
+	}
+	str = ft_arrdup(simple_cmd->str);
+	free_tools(tools);
+	determine_exit_code(str);
+	return (EXIT_SUCCESS);
+}
+ */
